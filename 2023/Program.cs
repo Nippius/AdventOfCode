40e3aba716a188a97ddc4cad8a3c66b5d67270bf
Nippius <Nippius@users.noreply.github.com>
2023-12-01 16:27:08 +0000

    Add day 1 solution for 2023

---
+++ 2023/Program.cs
@@ -0,0 +1,12 @@
+﻿using AdventOfCode;
+
+Adv01.Execute();
+// Adv02.Execute();
+// Adv03.Execute();
+// Adv04.Execute();
+// Adv05.Execute();
+// Adv06.Execute();
+// Adv07.Execute();
+// Adv08.Execute();
+// Adv09.Execute();
+// Adv10.Execute();
---
+++ 2023/adv1/Adv1.cs
@@ -0,0 +1,73 @@
+namespace AdventOfCode;
+
+public static class Adv01
+{
+    private static SortedList<int, int> FindAllDigitsInString(string s)
+    {
+        SortedList<int, int> numbers = [];
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (char.IsDigit(s[i]))
+            {
+                numbers.Add(i, s[i] - 48);
+            }
+        }
+        return numbers;
+    }
+
+    private static SortedList<int, int> FindAllDigitsAsTextInString(string s)
+    {
+        (string digitWord, int digit)[] digitsAsText = [
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four",4),
+            ("five",5),
+            ("six",6),
+            ("seven",7),
+            ("eight",8),
+            ("nine",9),
+            ("zero",0)
+        ];
+
+        SortedList<int, int> numbers = [];
+
+        foreach (var (digitWord, digit) in digitsAsText)
+        {
+            int foundIndex = -1;
+            int nextIndex = 0;
+            do
+            {
+                foundIndex = s.IndexOf(digitWord, nextIndex);
+                if (foundIndex > -1)
+                {
+                    numbers.Add(foundIndex, digit);
+                    nextIndex = foundIndex + digitWord.Length;
+                }
+            } while (foundIndex > -1);
+        }
+
+        return numbers;
+    }
+
+    public static void Execute()
+    {
+        int sum = 0;
+        StringReader sr = new(File.ReadAllText("./adv1/input.txt"));
+        string? line = sr.ReadLine();
+        while (line != null)
+        {
+            if (line != string.Empty)
+            {
+                SortedList<int, int> numbers = FindAllDigitsInString(line);
+                foreach(var (key, value) in FindAllDigitsAsTextInString(line)){
+                    numbers.Add(key, value);
+                }
+                sum += numbers.FirstOrDefault().Value*10+numbers.LastOrDefault().Value;
+            }
+            line = sr.ReadLine();
+        }
+
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
