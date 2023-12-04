eba7541d3f475f2c453c061fb060dcfe892e95cb
Nippius <Nippius@users.noreply.github.com>
2023-12-04 21:02:53 +0000

    Add initial script to create the template solution

---
+++ create_solution.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)][string]$Year = '2023'
+)
+
+foreach($day in 2..25)
+{
+    $formatedDay = '{0:d2}' -f $day
+    mkdir "day$formatedDay"
+    pushd "day$formatedDay"
+    $classContents =
+    @"
+namespace AdventOfCode$Year;
+
+public static class Day$formatedDay
+{
+    public static void Execute()
+    {
+        int sum = 0;
+        StringReader sr = new(File.ReadAllText("./day$formatedDay/input.txt"));
+        string? line = sr.ReadLine();
+        while (line != null)
+        {
+            if (line != string.Empty)
+            {
+                //TODO
+            }
+            line = sr.ReadLine();
+        }
+
+        Console.WriteLine($"[AoC $Year - Day $formatedDay - Part 1] Result: {sum}");
+        Console.WriteLine($"[AoC $Year - Day $formatedDay - Part 2] Result: {sum}");
+    }
+}
+"@
+    Set-Content -Path "Day$formatedDay.cs" -Value $classContents -Encoding 'utf8'
+    echo '' > input.txt
+    echo '' > problem.txt
+    popd
+}
