84fe898c73aad08dbfc5e31d13cec5862d1e89da
Nippius <Nippius@users.noreply.github.com>
2024-12-03 16:03:10 +0000

    [2024][Day03] Add solutions for part 1 and part 2

--- 2024/day03/Day03.cs
+++ 2024/day03/Day03.cs
@@ -1,22 +1,40 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode2024;
 
-public static class Day03
+public static partial class Day03
 {
+    [GeneratedRegex("""(?<inst>(?<op>mul)\((?<op1>\d+),(?<op2>\d+)\))|(?<op>do)\(\)|(?<op>don\'t)\(\)""")]
+    private static partial Regex CorruptedMemoryInstructionParser();
+
+    private static void ExecuteInstructionsInCorruptedMemory(string corruptedMemory, out int partOneResult, out int partTwoResult)
+    {
+        bool instructionsAreEnabled = true;
+        partOneResult = 0;
+        partTwoResult = 0;
+
+        foreach (Match instruction in CorruptedMemoryInstructionParser().Matches(corruptedMemory))
+        {
+            GroupCollection groups = instruction.Groups;
+            if (groups["op"].Value == "do") { instructionsAreEnabled = true; }
+            else if (groups["op"].Value == "don't") { instructionsAreEnabled = false; }
+            else
+            {
+                int instructionExecutionResult = int.Parse(groups["op1"].Value) * int.Parse(groups["op2"].Value);
+                partOneResult += instructionExecutionResult;
+                if(instructionsAreEnabled){ partTwoResult += instructionExecutionResult;}
+            }
+        }
+    }
+
     public static void Execute()
     {
-        int sum = 0;
-        using StringReader? sr = new(File.ReadAllText("./day03/input.txt"));
-        string? line = sr?.ReadLine();
-        while (line is not null)
-        {
-            if (line != string.Empty)
-            {
-                // TODO
-            }
-            line = sr?.ReadLine();
-        }
-        
-        Console.WriteLine($"[AoC 2024 - Day 03 - Part 1] Result: {sum}");
-        Console.WriteLine($"[AoC 2024 - Day 03 - Part 2] Result: {sum}");
+        // Assume the \n in the file are part of the corruption. Think of it as single long string
+        string memoryContents = File.ReadAllText("./day03/input.txt");
+
+        ExecuteInstructionsInCorruptedMemory(memoryContents, out int partOneResult, out int partTwoResult);
+
+        Console.WriteLine($"[AoC 2024 - Day 03 - Part 1] Result: {partOneResult}");
+        Console.WriteLine($"[AoC 2024 - Day 03 - Part 2] Result: {partTwoResult}");
     }
 }
