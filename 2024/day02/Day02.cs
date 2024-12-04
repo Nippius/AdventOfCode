8b08cdf766beaa9dbac6ad2183a556b1a5efd39b
Nippius <Nippius@users.noreply.github.com>
2024-12-04 00:14:14 +0000

    [2024][Day02] Add solutions for part 1 and part 2

--- 2024/day02/Day02.cs
+++ 2024/day02/Day02.cs
@@ -2,21 +2,66 @@ namespace AdventOfCode2024;
 
 public static class Day02
 {
+    private static bool LevelsAreSafe(IList<int> levels)
+    {
+        bool originalOrdering = levels[0] < levels[1]; // true -> ascending | false -> descending
+        for (int i = 1; i < levels.Count; i++)
+        {
+            int levelRange = Math.Abs(levels[i - 1] - levels[i]);
+            bool levelRangeOutsideLimits = levelRange < 1 || levelRange > 3;
+            bool orderChanged = originalOrdering != levels[i - 1] < levels[i];
+
+            if (levelRangeOutsideLimits || orderChanged)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static bool LevelsAreSafeWithTolerance(IList<int> levels)
+    {
+        // Bruteforce all possibilities.
+        // If we are here, we know the original report is not safe
+        //   so we remove each level and check until with find a safe report
+        for (int i = 0; i < levels.Count; i++)
+        {
+            if (LevelsAreSafe([.. levels.Where((_, j) => j != i)])) return true;
+        }
+        return false;
+    }
+
+    private static void CheckReportSafety(IList<int> levels, ref int safeReportCounter, ref int safeReportWithToleranceCounter)
+    {
+        bool levelsAreSafe = LevelsAreSafe(levels);
+
+        if (levelsAreSafe) { safeReportCounter++; }
+        if (levelsAreSafe || LevelsAreSafeWithTolerance(levels)) { safeReportWithToleranceCounter++; }
+    }
+
     public static void Execute()
     {
-        int sum = 0;
-        using StringReader? sr = new(File.ReadAllText("./day02/input.txt"));
-        string? line = sr?.ReadLine();
-        while (line is not null)
+        int safeReportCount = 0;
+        int safeReportCountWithTolerance = 0;
+        string inputContents = File.ReadAllText("./day02/input.txt");
+        using StringReader? sr = new(inputContents);
+        string? reportInput = sr?.ReadLine();
+
+        while (reportInput is not null)
         {
-            if (line != string.Empty)
+            if (reportInput != string.Empty)
             {
-                // TODO
+                // A report is a list of levels
+                IList<int> report = [.. reportInput.Split(' ').Select(int.Parse)];
+
+                CheckReportSafety(report, ref safeReportCount, ref safeReportCountWithTolerance);
+
+                reportInput = sr?.ReadLine();
             }
-            line = sr?.ReadLine();
         }
-        
-        Console.WriteLine($"[AoC 2024 - Day 02 - Part 1] Result: {sum}");
-        Console.WriteLine($"[AoC 2024 - Day 02 - Part 2] Result: {sum}");
+
+        Console.WriteLine($"[AoC 2024 - Day 02 - Part 1] Result: {safeReportCount}");
+        Console.WriteLine($"[AoC 2024 - Day 02 - Part 2] Result: {safeReportCountWithTolerance}");
     }
 }
