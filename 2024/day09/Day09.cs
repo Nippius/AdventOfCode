096ea2c47f2161a9e900a0fad39ebf8f6aabfd30
Nippius <Nippius@users.noreply.github.com>
2024-12-10 01:24:22 +0000

    [2024][Day09] Add solution for part 2

--- 2024/day09/Day09.cs
+++ 2024/day09/Day09.cs
@@ -2,40 +2,25 @@ namespace AdventOfCode2024;
 
 public static class Day09
 {
-    private record MySpan(int Start, int Length);
-    public static int[] ParseInput(StringReader sr)
-    {
-        string? line = sr?.ReadLine();
-        if (line is not null && line != string.Empty)
-        {
-            return Array.ConvertAll<char, int>([.. line], c => (int)char.GetNumericValue(c));
-        }
-        return [];
-    }
+    private const int EMPTY_BLOCK = -1;
 
-    private static void FillBlock(Span<int> block, int value)
+    public static int[] ParseInput(string input)
     {
-        for (int i = 0; i < block.Length; i++)
-        {
-            block[i] = value;
-        }
+        return Array.ConvertAll([.. input], c => (int)char.GetNumericValue(c));
     }
 
     private static void LayoutDiskContents(int[] diskMap, int[] disk)
     {
-        int fileId = 0;
-        int i = 0;
-        int writeHeadIdx = 0;
+        int fileId = 0, i = 0, writeHeadIdx = 0;
         do
         {
             if (i % 2 == 0)
             {
-                FillBlock(disk.AsSpan(writeHeadIdx, diskMap[i]), fileId++);
+                disk.AsSpan(writeHeadIdx, diskMap[i]).Fill(fileId++);
             }
             else
             {
-                // Use -1 to represent empty space
-                FillBlock(disk.AsSpan(writeHeadIdx, diskMap[i]), -1);
+                disk.AsSpan(writeHeadIdx, diskMap[i]).Fill(EMPTY_BLOCK);
             }
             writeHeadIdx += diskMap[i];
             i++;
@@ -48,14 +33,107 @@ public static class Day09
         int readHeadIdx = disk.Length - 1;
         while (writeHeadIdx <= readHeadIdx)
         {
-            if (disk[writeHeadIdx] != -1)
+            if (disk[writeHeadIdx] != EMPTY_BLOCK)
             {
                 writeHeadIdx++;
             }
             else
             {
                 disk[writeHeadIdx] = disk[readHeadIdx];
-                disk[readHeadIdx--] = -1;
+                disk[readHeadIdx--] = EMPTY_BLOCK;
+            }
+        }
+    }
+
+    private static bool TryFindGap(int[] disk, int fileStartIdx, int minGapSize, out Span<int> emptyGap)
+    {
+        emptyGap = [];
+        int gapStartIdx = 0;
+        if (gapStartIdx > fileStartIdx)
+        {
+            return false;
+        }
+        else
+        {
+            // Files can only be moved left and only if there's a big enough gap
+            //  so there's no point looking for gaps after the current file we are trying to move
+            while (gapStartIdx < fileStartIdx)
+            {
+                // Found the beginning of the gap
+                if (disk[gapStartIdx] == EMPTY_BLOCK)
+                {
+                    int gapEndIdx = gapStartIdx;
+                    while (gapEndIdx != disk.Length && disk[gapEndIdx] == EMPTY_BLOCK)
+                    { 
+                        gapEndIdx++; 
+                    }
+
+                    emptyGap = disk.AsSpan(gapStartIdx, gapEndIdx - gapStartIdx);
+                    if (emptyGap.Length >= minGapSize)
+                    {
+                        return true;
+                    }
+                }
+                gapStartIdx++;
+            }
+        }
+        return false;
+    }
+
+    private static bool FindNextFile(int[] disk, int startIdx, out Span<int> file)
+    {
+        file = [];
+
+        if (startIdx < 0)
+        {
+            return false;
+        }
+        else
+        {
+            while (startIdx >= 0)
+            {
+                // Found beginning of the file
+                if (disk[startIdx] != EMPTY_BLOCK)
+                {
+                    int fileId = disk[startIdx]; // Because files could be back to back with no empty space in between
+                    int endIdx = startIdx;
+                    while (endIdx >= 0 && disk[endIdx] == fileId)
+                    {
+                        endIdx--;
+                    }
+
+                    file = disk.AsSpan(endIdx + 1, startIdx - endIdx);
+
+                    return true;
+                }
+                startIdx--;
+            }
+        }
+        return false;
+    }
+
+    private static void CompactDiskFiles(int[] disk)
+    {
+        int readHeadIdx = disk.Length - 1;
+
+        while (FindNextFile(disk, readHeadIdx, out Span<int> file))
+        {
+            // We search files backwards. ence the weird arithematic. This wouls actually be the start of the file
+            int fileEndIdx = readHeadIdx - file.Length + 1;
+            while (TryFindGap(disk, fileEndIdx, file.Length, out Span<int> emptyGap))
+            {
+                if (file.Length <= emptyGap.Length)
+                {
+                    file.CopyTo(emptyGap);
+                    file.Fill(EMPTY_BLOCK);
+                    break;
+                }
+            }
+
+            readHeadIdx -= file.Length;
+            while (readHeadIdx >= 0 && disk[readHeadIdx] == EMPTY_BLOCK)
+            {
+                readHeadIdx--;
             }
         }
     }
@@ -65,28 +143,27 @@ public static class Day09
         long checksum = 0;
         for (int i = 0; i < disk.Length; i++)
         {
-            if (disk[i] != -1) // -1 =  empty block
+            if (disk[i] == EMPTY_BLOCK) // -1 =  empty block
             {
-                checksum += i * disk[i];
+                continue;
             }
+            checksum += i * disk[i];
         }
         return checksum;
     }
 
     public static void Execute()
     {
-        using StringReader? sr = new(File.ReadAllText("./day09/input.txt"));
+        int[] diskMap = ParseInput(File.ReadAllText("./day09/input.txt"));
 
-        int[] diskMap = ParseInput(sr);
         int[] disk = new int[diskMap.Sum()];
+        int[] diskCopy = new int[disk.Length];
 
         LayoutDiskContents(diskMap, disk);
-
-        int[] diskCopy = new int[diskMap.Sum()];
         Array.Copy(disk, diskCopy, disk.Length);
 
         CompactDiskBlocks(disk);
-        //CompactDiskFiles(diskCopy);
+        CompactDiskFiles(diskCopy);
 
         Console.WriteLine($"[AoC 2024 - Day 09 - Part 1] Result: {CalculateDiskChecksum(disk)}");
         Console.WriteLine($"[AoC 2024 - Day 09 - Part 2] Result: {CalculateDiskChecksum(diskCopy)}");
