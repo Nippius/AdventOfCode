6d3671bb38d3957a7719d9dc72e2a97b2a561428
Nippius <Nippius@users.noreply.github.com>
2024-12-13 23:12:23 +0000

    [2024][Day13] Add solutions for part 1 and part 2

--- 2024/day13/Day13.cs
+++ 2024/day13/Day13.cs
@@ -1,22 +1,69 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode2024;
 
-public static class Day13
+public static partial class Day13
 {
-    public static void Execute()
+    [GeneratedRegex("""\d+""")]
+    private static partial Regex NumberParser();
+
+    private record Machine(long AX, long AY, long BX, long BY, long PX, long PY);
+
+    private static List<Machine> ParseInput(StringReader sr)
     {
-        int sum = 0;
-        using StringReader? sr = new(File.ReadAllText("./day13/input.txt"));
+        List<Machine> machines = [];
         string? line = sr?.ReadLine();
         while (line is not null)
         {
             if (line != string.Empty)
             {
-                // TODO
+                MatchCollection numbers = NumberParser().Matches(line);
+                long aX = long.Parse(numbers[0].Value);
+                long aY = long.Parse(numbers[1].Value);
+
+                line = sr?.ReadLine();
+                numbers = NumberParser().Matches(line!);
+                long bX = long.Parse(numbers[0].Value);
+                long bY = long.Parse(numbers[1].Value);
+
+                line = sr?.ReadLine();
+                numbers = NumberParser().Matches(line!);
+                long pX = long.Parse(numbers[0].Value);
+                long pY = long.Parse(numbers[1].Value);
+                
+                machines.Add(new Machine(aX, aY, bX, bY, pX, pY));
             }
             line = sr?.ReadLine();
         }
-        
-        Console.WriteLine($"[AoC 2024 - Day 13 - Part 1] Result: {sum}");
-        Console.WriteLine($"[AoC 2024 - Day 13 - Part 2] Result: {sum}");
+        return machines;
     }
+
+    // Using Cramer's rule
+    // https://www.youtube.com/watch?v=vXqlIOX2itM
+    private static long CalculateTokenCounts(List<Machine> machines)
+    {
+        long tokenCount = 0;
+        foreach (Machine machine in machines)
+        {
+            long det = machine.AX * machine.BY - machine.AY * machine.BX;
+            long A = (machine.PX * machine.BY - machine.PY * machine.BX) / det;
+            long B = (machine.AX * machine.PY - machine.AY * machine.PX) / det;
+            if(machine.AX * A + machine.BX * B == machine.PX && machine.AY * A + machine.BY * B == machine.PY){
+                tokenCount += 3 * A + B;
+            }
+        }
+        return tokenCount;
+    }
+
+    public static void Execute()
+    {
+        using StringReader? sr = new(File.ReadAllText("./day13/input.txt"));
+
+        List<Machine> machines = ParseInput(sr);
+        List<Machine> machinesWithOffset = [.. machines.Select(m => m with { PX = m.PX + 10000000000000, PY = m.PY + 10000000000000 })];
+
+        Console.WriteLine($"[AoC 2024 - Day 13 - Part 1] Result: {CalculateTokenCounts(machines)}");
+        Console.WriteLine($"[AoC 2024 - Day 13 - Part 2] Result: {CalculateTokenCounts(machinesWithOffset)}");
+    }
+
 }
