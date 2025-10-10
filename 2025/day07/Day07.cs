6039f4ef2110465482adbb331b3c88ca2b06836c
Nippius <Nippius@users.noreply.github.com>
2025-10-10 02:21:59 +0100

    [2025][Day00] Add initial project and solution

---
+++ 2025/day07/Day07.cs
@@ -0,0 +1,42 @@
+namespace AdventOfCode2025.Day07;
+
+public static class Day07
+{
+    private static List<string> ParseInput(StringReader sr){
+        using(sr)
+        {
+            List<string> input = [];
+            string? line = sr?.ReadLine();
+            while (line is not null)
+            {
+                if (line != string.Empty)
+                {
+                    input.Add(line);
+                }
+                line = sr?.ReadLine();
+            }
+            return input;
+        }
+    }
+
+    private static int Part1(int sum)
+    {
+        return sum;
+    }
+
+    private static int Part2(int sum)
+    {
+        return sum;
+    }
+
+    public static void Execute()
+    {
+        int sum = 0;
+        using StringReader? sr = new(File.ReadAllText("./day07/input.txt"));
+        
+        List<string> input = ParseInput(sr);
+        
+        Console.WriteLine($"[AoC 2025 - Day 07 - Part 1] Result: {Part1(sum)}");
+        Console.WriteLine($"[AoC 2025 - Day 07 - Part 2] Result: {Part2(sum)}");
+    }
+}
---
+++ 2025/day25/Day25.cs
@@ -0,0 +1,42 @@
+namespace AdventOfCode2025.Day25;
+
+public static class Day25
+{
+    private static List<string> ParseInput(StringReader sr){
+        using(sr)
+        {
+            List<string> input = [];
+            string? line = sr?.ReadLine();
+            while (line is not null)
+            {
+                if (line != string.Empty)
+                {
+                    input.Add(line);
+                }
+                line = sr?.ReadLine();
+            }
+            return input;
+        }
+    }
+
+    private static int Part1(int sum)
+    {
+        return sum;
+    }
+
+    private static int Part2(int sum)
+    {
+        return sum;
+    }
+
+    public static void Execute()
+    {
+        int sum = 0;
+        using StringReader? sr = new(File.ReadAllText("./day25/input.txt"));
+        
+        List<string> input = ParseInput(sr);
+        
+        Console.WriteLine($"[AoC 2025 - Day 25 - Part 1] Result: {Part1(sum)}");
+        Console.WriteLine($"[AoC 2025 - Day 25 - Part 2] Result: {Part2(sum)}");
+    }
+}
