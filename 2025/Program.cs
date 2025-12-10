3c74fbb8ed0836d579c8a84fb227a7ac51cb00a1
Nippius <Nippius@users.noreply.github.com>
2025-12-10 00:02:01 +0000

    [2025][Day05] Add solutions for part 1 and part 2

--- 2025/Program.cs
+++ 2025/Program.cs
@@ -24,11 +24,11 @@ using AdventOfCode2025.Day23;
 using AdventOfCode2025.Day24;
 using AdventOfCode2025.Day25;
 
-// Day01.Execute();
-// Day02.Execute();
-// Day03.Execute();
+Day01.Execute();
+Day02.Execute();
+Day03.Execute();
 Day04.Execute();
-// Day05.Execute();
+Day05.Execute();
 // Day06.Execute();
 // Day07.Execute();
 // Day08.Execute();
--- 2025/day05/Day05.cs
+++ 2025/day05/Day05.cs
@@ -2,41 +2,123 @@ namespace AdventOfCode2025.Day05;
 
 public static class Day05
 {
-    private static List<string> ParseInput(StringReader sr){
-        using(sr)
+    private class IngredientsDatabase(List<IngredientsDatabase.IdRange> idRanges, List<long> availableIngredients)
+    {
+        public record IdRange(long min, long max); // Ranges are inclusive
+        public List<IdRange> FreshIngredientRanges { get; private set; } = idRanges;
+        public List<long> AvailableIngredients { get; private set; } = availableIngredients;
+    }
+
+    private static IngredientsDatabase ParseInput(StringReader sr)
+    {
+        using (sr)
         {
-            List<string> input = [];
+            List<IngredientsDatabase.IdRange> idRanges = [];
+            List<long> availableIngredients = [];
             string? line = sr?.ReadLine();
             while (line is not null)
             {
                 if (line != string.Empty)
                 {
-                    input.Add(line);
+                    var temp = line.Split('-');
+                    if (temp.Length == 2)
+                    {
+                        idRanges.Add(new(long.Parse(temp[0]), long.Parse(temp[1])));
+                    }
+                    else
+                    {
+                        availableIngredients.Add(long.Parse(temp[0]));
+                    }
                 }
                 line = sr?.ReadLine();
             }
-            return input;
+            return new IngredientsDatabase(idRanges, availableIngredients);
         }
     }
 
-    private static int Part1(int sum)
+    private static int Part1(IngredientsDatabase database)
     {
-        return sum;
+        int freshIngredients = 0;
+        foreach (long id in database.AvailableIngredients)
+        {
+            bool ingredientInAtLeastOneRange = false;
+            foreach (IngredientsDatabase.IdRange range in database.FreshIngredientRanges)
+            {
+                if (id >= range.min && id <= range.max)
+                {
+                    ingredientInAtLeastOneRange = true;
+                }
+            }
+            if (ingredientInAtLeastOneRange)
+            {
+                freshIngredients++;
+            }
+        }
+        return freshIngredients;
     }
 
-    private static int Part2(int sum)
+    private static long Part2(IngredientsDatabase database)
     {
-        return sum;
+        long validIds = 0;
+
+        IngredientsDatabase.IdRange[] freshIngredientRanges = [.. database.FreshIngredientRanges];
+        freshIngredientRanges.Sort((x, y) => (x?.min >= y?.min) ? 1 : -1);
+        List<IngredientsDatabase.IdRange> mergedRanges = [];
+
+        for (int i = 0; i < freshIngredientRanges.Length - 1;)
+        {
+            var currentRange = freshIngredientRanges[i];
+            int lastMergedRangeIdx = 0;
+            // Merge the next ranges as many times as we can
+            for (int j = i + 1; j < freshIngredientRanges.Length; j++)
+            {
+                var possibleRangeToMerge = freshIngredientRanges[j];
+
+                // Since ranges are ordered, we know that after this point they can't be merged
+                if (possibleRangeToMerge.min > currentRange.max)
+                {
+                    lastMergedRangeIdx = j;
+                    break;
+                }
+
+                // Expand range by merging with previous range
+                if (possibleRangeToMerge.min >= currentRange.min && possibleRangeToMerge.max >= currentRange.max)
+                {
+                    // Ranges are sorted by min so the possibleRangeToMerge either ends "inside" the current
+                    // range, which we can ignore, or ends "outside" the current range and so we merge
+                    // with the currentRange expanding it.
+                    currentRange = new(currentRange.min, possibleRangeToMerge.max);
+                    lastMergedRangeIdx = j;
+                }
+            }
+
+            mergedRanges.Add(currentRange);
+
+            if (lastMergedRangeIdx == 0) // If zero, no ranges were merged so we move on to the next range
+            {
+                i++;
+            }
+            else // else move to the next range that wasn't merged
+            {
+                i = lastMergedRangeIdx;
+            }
+        }
+
+        foreach (var range in mergedRanges)
+        {
+            validIds += range.max - range.min + 1;
+        }
+
+        return validIds;
     }
 
     public static void Execute()
     {
-        int sum = 0;
         using StringReader? sr = new(File.ReadAllText("./day05/input.txt"));
-        
-        List<string> input = ParseInput(sr);
-        
-        Console.WriteLine($"[AoC 2025 - Day 05 - Part 1] Result: {Part1(sum)}");
-        Console.WriteLine($"[AoC 2025 - Day 05 - Part 2] Result: {Part2(sum)}");
+
+        IngredientsDatabase ingredientsDatabase = ParseInput(sr);
+
+        Console.WriteLine($"[AoC 2025 - Day 05 - Part 1] Result: {Part1(ingredientsDatabase)}");
+        Console.WriteLine($"[AoC 2025 - Day 05 - Part 2] Result: {Part2(ingredientsDatabase)}");
     }
 }
